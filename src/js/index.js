04c864a35f9f62fe82282b20096c826be0006b91
salmoncode <salmon2073@gmail.com>
2022-08-11 15:09:03 +0900

    init

---
+++ src/js/index.js
@@ -0,0 +1,64 @@
+const chatLog = []
+
+const apiKey = "23d763e7-f3dc-403a-81ae-9ce014c4b9d8"
+
+let peer
+let room
+
+const joinRoom = async () => {
+  const localStream = await await navigator.mediaDevices.getUserMedia({audio: true, video: true})
+  const localVideoElm = document.querySelector(".local-stream")
+  localVideoElm.srcObject = localStream
+  localVideoElm.play()
+
+  peer = new Peer({key: apiKey})
+  peer.on("open", () => {
+    room = peer.joinRoom("video-chat-with-subtitles", {
+      mode: "sfu",
+      stream: localStream,
+    })
+
+    room.on("stream", stream => {
+      console.log("stream")
+      const remoteVideoElm = document.querySelector(".remote-stream")
+      remoteVideoElm.srcObject = stream
+      remoteVideoElm.play()
+    })
+
+    room.on("data", ({ src, data }) => {
+      applyText(data)
+    })
+  })
+}
+
+const startRecognition = () => {
+  SpeechRecognition = webkitSpeechRecognition || SpeechRecognition
+  const recognition = new SpeechRecognition()
+  recognition.continuous = true
+
+  recognition.onresult = (event) => {
+    const results = event.results
+    const text = results[results.length - 1][0].transcript
+    applyText(`あなた: ${text}`)
+    send(`${peer.id}: ${text}`);
+  }
+
+  recognition.start()
+}
+
+const applyText = (text) => {
+  chatLog.push(text)
+  const textAreaElm = document.querySelector("textarea")
+  textAreaElm.value = chatLog.join("\n")
+}
+
+const send = text => {
+  room.send(text);
+}
+
+const main = async () => {
+  await joinRoom()
+  startRecognition()
+}
+
+window.onload = main
